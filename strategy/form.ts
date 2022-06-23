2609f2c78dd100c988d226b394fa1403e6823b2e
cuishunbiao <860026526@qq.com>
2022-06-23 08:49:53 +0800

    updata code

--- strategy/form.ts
+++ strategy/form.ts
@@ -8,49 +8,63 @@ const start1 = 3, end1 = 1;
 const start2 = '1212172718', end2 = 1;
 const start3 = 5, end3 = 1;
 
-let errorResult = []
-const isMoreFn = (start1, end1, text) => {
-    if (!start1 || !end2) return
-    if (start1 > end1) errorResult.push(text);
-}
-const checkFn = (fn, arg) => {
-    return fn.apply(this, arg)
-}
-
-checkFn(isMoreFn, [start1, end1, '这个验证是错误的'])
-checkFn(isMoreFn, [start3, end3, '这个验证是错误的1'])
-console.log(errorResult)
-
-
-
-// const formDataCheck = {
-//     dataErrorText: [],
-//     isMore: function (start, end, errorText) {
-//         if (!start || !end) return;
-//         if (start > end) {
-//             this.dataErrorText.push(errorText)
-//             return;
-//         }
-//     },
-//     isLess: function (start, end, errorText) {
-//         if (!start || !end) return;
-//         if (start < end) {
-//             this.dataErrorText.push(errorText)
-//             return;
-//         }
-//     },
-//     isLengthFn: function (str, len, errorText) {
-//         const _str = str.toString()
-//         if (!_str) return;
-//         if (_str.length < len) {
-//             this.dataErrorText.push(errorText);
-//             return;
-//         }
-//     },
-//     errorResult: function () {
-//         return Array.from(new Set(this.dataErrorText));
-//     }
+// let errorResult = []
+// const isMoreFn = (start1, end1, text) => {
+//     if (!start1 || !end2) return
+//     if (start1 > end1) errorResult.push(text);
 // }
+// const checkFn = (fn, arg) => {
+//     return fn.apply(this, arg)
+// }
+
+// checkFn(isMoreFn, [start1, end1, '这个验证是错误的'])
+// checkFn(isMoreFn, [start3, end3, '这个验证是错误的1'])
+// console.log(errorResult)
+
+interface IErrorResult {
+    dataErrorText: string[],
+    errorResult: () => string[]
+}
+
+const errorResult: IErrorResult = {
+    dataErrorText: [],
+    errorResult: function () {
+        return Array.from(new Set(this.dataErrorText));
+    }
+}
+
+const formDataCheck = {
+    isMore: function (start, end, errorText: string) {
+        if (!start || !end) return;
+        if (start > end) {
+            errorResult.dataErrorText.push(errorText)
+            return;
+        }
+    },
+    isLess: function (start, end, errorText) {
+        if (!start || !end) return;
+        if (start < end) {
+            errorResult.dataErrorText.push(errorText)
+            return;
+        }
+    },
+    isLengthFn: function (str, len, errorText) {
+        const _str = str.toString()
+        if (!_str) return;
+        if (_str.length < len) {
+            errorResult.dataErrorText.push(errorText)
+            return;
+        }
+    }
+}
+
+const calculateBouns = function (level, arg) {
+    return formDataCheck[level].apply(formDataCheck, arg);
+};
+
+calculateBouns('isMore', [start1, end1, '错误1'])
+calculateBouns('isMore', [start2, end2, '错误2'])
+console.log(errorResult.errorResult().join(";\n"));
 
 // //开始执行
 // formDataCheck.isMore(start1, end1, '错误1');
